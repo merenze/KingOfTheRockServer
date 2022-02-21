afde0f2cba282f3d3fd86680f4219d251c30e208
Matthew Renze <merenze@gmail.com>
2022-02-21 13:56:18 -0600

    Fix UserRepository JPA connection

--- Backend/src/main/java/coms309/s1yn3/backend/entity/repository/UserRepository.java
+++ Backend/src/main/java/coms309/s1yn3/backend/entity/repository/UserRepository.java
@@ -3,7 +3,10 @@ package coms309.s1yn3.backend.entity.repository;
 import coms309.s1yn3.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 	User findById(int id);
-	User deleteById();
+	@Transactional
+	void deleteById(int id);
 }
