a2f38147fcce9c8528bb7a826fc8c2865de8b0e3
Noah Cordova <15nocordo15@gmail.com>
2022-02-23 23:16:40 -0600

    login screen with some logic errors

---
+++ Frontend/LoginScreen/app/src/main/java/com/example/loginscreen/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameOrEmail;
+    private EditText password;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameOrEmail = (EditText)findViewById(R.id.activity_main_et_name);
+        password = (EditText)findViewById(R.id.activity_main_et_password);
+        loginButton = (Button)findViewById(R.id.activity_main_button_login);
+
+        loginButton.setOnClickListener(new OnClickListener()){
+            @Override
+            public void onClick(View view)
+            {
+                validateLogin(usernameOrEmail.getText().toString(), password.getText().toString());
+            }
+        }
+    }
+
+    /*
+    private void validateLogin(String databaseUsername, String databaseEmail, String databasePassword){
+        //if ((usernameOrEmail.equals(databaseUsername) || usernameOrEmail.equals(databaseEmail)) && password.equals(databasePassword))
+        if ((usernameOrEmail.equals("noahcordova") || usernameOrEmail.equals(databaseEmail)) && password.equals("password"))
+        {
+            Intent intent = new Intent(MainActivity.this, AfterLoginScreen.class);
+            startActivity(intent);
+        }
+    }
+    */
+    private void validateLogin(String databaseUsername, String databasePassword){
+        //if ((usernameOrEmail.equals(databaseUsername) || usernameOrEmail.equals(databaseEmail)) && password.equals(databasePassword))
+        if (usernameOrEmail.equals("noahcordova") && password.equals("password"))
+        {
+            Intent intent = new Intent(MainActivity.this, AfterLoginScreen.class);
+            startActivity(intent);
+        }
+    }
+
+}
