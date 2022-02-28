57fde3e610d0a98d95b4722eb2dd8494f327932b
Matthew Renze <merenze@gmail.com>
2022-02-27 21:39:44 -0600

    Add user login validation

--- Backend/src/main/java/coms309/s1yn3/backend/controller/UserController.java
+++ Backend/src/main/java/coms309/s1yn3/backend/controller/UserController.java
@@ -2,15 +2,24 @@ package coms309.s1yn3.backend.controller;
 
 import coms309.s1yn3.backend.entity.User;
 import coms309.s1yn3.backend.entity.repository.UserRepository;
+import coms309.s1yn3.backend.service.UserProviderService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class UserController {
 	@Autowired
 	UserRepository users;
+
+	@Autowired
+	UserProviderService userProvider;
 
 	@GetMapping("/users")
 	public @ResponseBody List<User> index() {
@@ -49,5 +58,22 @@ public class UserController {
 		}
 		users.deleteById(id);
 		return "Success";
+	}
+
+	@GetMapping("/login")
+	public @ResponseBody ResponseEntity login(@RequestBody Map<String, String> request) {
+		String username = request.get("username");
+		String password = request.get("password");
+		User user = userProvider.getLoginUser(username, password);
+		if (user == null) {
+			return new ResponseEntity(
+					"Username or password incorrect.",
+					HttpStatus.NOT_FOUND
+			);
+		}
+		return new ResponseEntity(
+				user,
+				HttpStatus.OK
+		);
 	}
 }
--- Backend/src/main/java/coms309/s1yn3/backend/entity/repository/UserRepository.java
+++ Backend/src/main/java/coms309/s1yn3/backend/entity/repository/UserRepository.java
@@ -3,12 +3,17 @@ package coms309.s1yn3.backend.entity.repository;
 import coms309.s1yn3.backend.entity.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @SpringBootApplication
 public interface UserRepository extends JpaRepository<User, Integer> {
 	User findById(int id);
 	@Transactional
 	void deleteById(int id);
+
+	@Query("SELECT new User(u.id,  u.email, u.username, u.password, u.isAdmin) FROM User u WHERE u.username = ?1 AND u.password = ?2")
+	List<User> getUsers(String username, String password);
 }
