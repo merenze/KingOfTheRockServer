8cec4dc967d150c4568b9a823d44e5d7408f5ba7
Matthew Renze <merenze@gmail.com>
2022-03-09 17:56:16 -0600

    Register auth filter to proper routes

--- Backend/src/main/java/coms309/s1yn3/backend/filter/AuthFilter.java
+++ Backend/src/main/java/coms309/s1yn3/backend/filter/AuthFilter.java
@@ -2,9 +2,7 @@ package coms309.s1yn3.backend.filter;
 
 import coms309.s1yn3.backend.service.SessionProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -14,16 +12,22 @@ import java.io.IOException;
 /**
  * Requires request sender to have a valid user session.
  */
-@Component
-@Order(1)
 public class AuthFilter implements Filter {
-	@Autowired SessionProviderService sessions;
+	private SessionProviderService sessions;
+
+	public AuthFilter(SessionProviderService sessions) {
+		this.sessions = sessions;
+	}
+
 	@Override public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
 		HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
 		String header = httpRequest.getHeader("auth-token");
 		if (header == null || sessions.getUser(header) == null) {
 			httpResponse.setStatus(HttpStatus.FORBIDDEN.value());
+			return;
 		}
+
+		filterChain.doFilter(servletRequest, servletResponse);
 	}
 }
