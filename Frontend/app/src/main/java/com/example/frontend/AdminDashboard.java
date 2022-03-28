6508bf0f5ff97ecc392fc7b90603523088428e78
Daniel Rosenhamer <djr2@iastate.edu>
2022-03-28 15:32:09 -0500

    User list button setup

--- Frontend/app/src/main/java/com/example/frontend/AdminDashboard.java
+++ Frontend/app/src/main/java/com/example/frontend/AdminDashboard.java
@@ -15,6 +15,14 @@ public class AdminDashboard extends AppCompatActivity {
         setContentView(R.layout.activity_admin_dashboard);
 
         //buttons here
+
+        Button buttonToUserList = findViewById(R.id.admin_dashboard_user_list_button);
+        buttonToUserList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), UserListScreen.class));
+            }
+        });
     }
 
 }
