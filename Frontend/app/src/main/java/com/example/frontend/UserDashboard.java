021cdf50735f147e8eceb023a5720795e95850f1
Daniel Rosenhamer <djr2@iastate.edu>
2022-03-28 15:41:31 -0500

    Find button implemented

---
+++ Frontend/app/src/main/java/com/example/frontend/UserDashboard.java
@@ -0,0 +1,24 @@
+package com.example.frontend;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+        Button buttonToFindUser = findViewById(R.id.user_dashboard_find_user_button);
+        buttonToFindUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), SearchForUserScreen.class));
+            }
+        });
+    }
+}
