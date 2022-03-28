b31cb6300f4183bb521368d3c0bfa82894a3edab
Daniel Rosenhamer <djr2@iastate.edu>
2022-03-28 09:12:36 -0500

    Created buttons for the admin dashboard

---
+++ Frontend/app/src/main/java/com/example/frontend/AdminDashboard.java
@@ -0,0 +1,20 @@
+package com.example.frontend;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+
+        //buttons here
+    }
+
+}
