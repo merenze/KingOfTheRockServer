f9845de22f1f2f5a04b4970ef015e6e82350b2d9
Matthew Renze <merenze@gmail.com>
2022-04-02 14:03:46 -0500

    Add Game entity

---
+++ Backend/src/main/java/coms309/s1yn3/backend/entity/Game.java
@@ -0,0 +1,21 @@
+package coms309.s1yn3.backend.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Game {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
