a784d09c2b31863c8a22cef3c866998acd67103c
Matthew Renze <merenze@gmail.com>
2022-04-03 17:57:43 -0500

    Better docs for Game

--- Backend/src/main/java/coms309/s1yn3/backend/entity/Game.java
+++ Backend/src/main/java/coms309/s1yn3/backend/entity/Game.java
@@ -7,25 +7,47 @@ import java.util.List;
 
 @Entity
 public class Game {
+	/**
+	 * Unique ID for this game.
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	/**
+	 * List of game-user relations associated with this game.
+	 */
 	@OneToMany(targetEntity = GameUserRelation.class, mappedBy = "game")
 	private List<GameUserRelation> userRelations;
 
+	/**
+	 * @return Unique ID for this game.
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * For use by JPA.
+	 * Don't use this.
+	 * @param id Unique ID for this game.
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return List of game-user relations associated with this game.
+	 */
 	public List<GameUserRelation> getUserRelations() {
 		return userRelations;
 	}
 
+	/**
+	 * For use by JPA.
+	 * Don't use this.
+	 * @param userRelations
+	 */
 	public void setUserRelations(List<GameUserRelation> userRelations) {
 		this.userRelations = userRelations;
 	}
