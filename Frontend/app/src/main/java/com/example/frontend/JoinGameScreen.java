0e1a112b410827fd250e967cdf4cbd0e68141474
Daniel Rosenhamer <djr2@iastate.edu>
2022-04-04 00:38:34 -0500

    Join game elements implemented

--- Frontend/app/src/main/java/com/example/frontend/JoinGameScreen.java
+++ Frontend/app/src/main/java/com/example/frontend/JoinGameScreen.java
@@ -1,18 +1,43 @@
 package com.example.frontend;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JoinGameScreen extends AppCompatActivity {
 
-    private String lobbyCode;
+    private String lobbyCode = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
+        Button buttonToQuickPlay = (Button) findViewById(R.id.join_game_quick_play_button);
+
+        buttonToQuickPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+                //backend sends response containing a public lobby code
+                //attempt to join lobby
+            }
+        });
+
+        Button buttonToSubmitCode = (Button) findViewById(R.id.join_game_submit_code_button);
+
+        buttonToSubmitCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText etLobbyCode = (EditText) findViewById(R.id.join_game_code_text);
+                lobbyCode = etLobbyCode.getText().toString().trim();
+                //attempt to join lobby
+            }
+        });
 
     }
 }
