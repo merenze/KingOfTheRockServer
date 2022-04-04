82694f410cd8b6cd56fecbaed0b2b858eb1371f7
Noah Cordova <15nocordo15@gmail.com>
2022-04-04 14:40:09 -0500

    added cancel button logic

--- Frontend/app/src/main/java/com/example/frontend/TradeScreen.java
+++ Frontend/app/src/main/java/com/example/frontend/TradeScreen.java
@@ -2,6 +2,7 @@ package com.example.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -83,7 +84,7 @@ public class TradeScreen extends AppCompatActivity {
         woodPlusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //cancel logic
+                startActivity(new Intent(view.getContext(), GameViewScreen.class));
             }
         });
 
