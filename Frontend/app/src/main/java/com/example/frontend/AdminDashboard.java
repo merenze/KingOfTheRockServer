a5853fb2d1d72295c5f623616d2a5ef4fd61af63
Daniel Rosenhamer <djr2@iastate.edu>
2022-04-13 10:30:57 -0500

    Added button logic for both dashboards

--- Frontend/app/src/main/java/com/example/frontend/AdminDashboard.java
+++ Frontend/app/src/main/java/com/example/frontend/AdminDashboard.java
@@ -17,10 +17,20 @@ public class AdminDashboard extends AppCompatActivity {
         //buttons here
 
         Button buttonToUserList = findViewById(R.id.admin_dashboard_user_list_button);
+
         buttonToUserList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(view.getContext(), UserListScreen.class));
+            }
+        });
+
+        Button buttonToJoinGame = findViewById(R.id.admin_dashboard_join_game_button);
+
+        buttonToJoinGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), JoinGameScreen.class));
             }
         });
     }
