e450d7bf488f18bed048773abe18371b8050117f
Noah Cordova <15nocordo15@gmail.com>
2022-04-20 15:33:25 -0500

    created User entity

---
+++ Frontend/app/src/main/java/com/example/frontend/Entities/IUser.java
@@ -0,0 +1,15 @@
+package com.example.frontend.Entities;
+
+public interface IUser {
+    public String getUsername();
+
+    public void setUsername(String username);
+
+    public String getAuthToken();
+
+    public void setAuthToken(String authToken);
+
+    public boolean getIsAdmin();
+
+    public void setIsAdmin(boolean isAdmin);
+}
