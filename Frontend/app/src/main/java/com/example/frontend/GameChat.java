917e78ebe6add8c648e5b5aaa35f9303391f8b0e
Daniel Rosenhamer <djr2@iastate.edu>
2022-04-21 19:57:32 -0500

    Removed class from other class to make public

--- Frontend/app/src/main/java/com/example/frontend/GameChat.java
+++ Frontend/app/src/main/java/com/example/frontend/GameChat.java
@@ -13,31 +13,4 @@ public class GameChat extends AppCompatActivity {
     }
 }
 
-public class MemberData {
-    private String name;
-    private String color;
 
-    public MemberData(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
-
-    public MemberData(){
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    @Override
-    public String toString() {
-        return "MemberData{" +
-                "name'" + name +'\'' +
-                ", color='" + color + '\'' +
-                '}';
-    }
-}
