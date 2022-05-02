1be7edd538572b5b68a58680bb4b5786a4191a97
danielr3122 <drosenhamer@gmail.com>
2022-05-01 20:10:45 -0500

    Reduced code load

--- Frontend/app/src/main/java/com/example/frontend/AdminDashboard.java
+++ Frontend/app/src/main/java/com/example/frontend/AdminDashboard.java
@@ -19,33 +19,18 @@ public class AdminDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
 
-        //buttons here
-
         Button buttonToUserList = findViewById(R.id.admin_dashboard_user_list_button);
-        buttonToUserList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), UserListScreen.class));
-            }
-        });
+        buttonToUserList.setOnClickListener(view -> startActivity(new Intent(view.getContext(), UserListScreen.class)));
 
         Button buttonToJoinGame = findViewById(R.id.admin_dashboard_join_game_button);
 
-        buttonToJoinGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), JoinGameScreen.class));
-            }
-        });
+        buttonToJoinGame.setOnClickListener(view -> startActivity(new Intent(view.getContext(), JoinGameScreen.class)));
 
         Button buttonToHostGame = findViewById(R.id.admin_dashboard_host_game_button);
-        buttonToHostGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), Lobby.class);
-                intent.putExtra("isHost", true);
-                startActivity(intent);
-            }
+        buttonToHostGame.setOnClickListener(view -> {
+            Intent intent = new Intent(getBaseContext(), Lobby.class);
+            intent.putExtra("isHost", true);
+            startActivity(intent);
         });
     }
 
