da6958ac4d675cf925b8480ed87b0e39bf4e2938
danielr3122 <drosenhamer@gmail.com>
2022-05-01 20:10:06 -0500

    Reduced code load

--- Frontend/app/src/main/java/com/example/frontend/UserDashboard.java
+++ Frontend/app/src/main/java/com/example/frontend/UserDashboard.java
@@ -2,7 +2,6 @@ package com.example.frontend;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,30 +21,18 @@ public class UserDashboard extends AppCompatActivity {
 
         Button buttonToFindUser = findViewById(R.id.user_dashboard_find_user_button);
 
-        buttonToFindUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), SearchForUserScreen.class));
-            }
-        });
+        buttonToFindUser.setOnClickListener(view -> startActivity(new Intent(view.getContext(), SearchForUserScreen.class)));
 
         Button buttonToJoinGame = findViewById(R.id.user_dashboard_join_game_button);
 
-        buttonToJoinGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), JoinGameScreen.class));
-            }
-        });
+        buttonToJoinGame.setOnClickListener(view -> startActivity(new Intent(view.getContext(), JoinGameScreen.class)));
 
         Button buttonToHostGame = findViewById(R.id.user_dashboard_host_game_button);
-        buttonToHostGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), Lobby.class);
-                intent.putExtra("isHost", true);
-                startActivity(intent);
-            }
+
+        buttonToHostGame.setOnClickListener(view -> {
+            Intent intent = new Intent(getBaseContext(), Lobby.class);
+            intent.putExtra("isHost", true);
+            startActivity(intent);
         });
     }
 }
